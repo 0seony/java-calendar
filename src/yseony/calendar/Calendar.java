c3afa8fe596c6faa24a46c49617bc8b35da9502b
Yeongseon Park <yseon406@gmail.com>
2022-11-04 15:25:23 +0900

    refactor : 최대 일수 출력, 캘린더 출력 메서드화
    최대 일수 출력과 캘린더 출력을 메서드로 분리

--- src/yseony/calendar/Calendar.java
+++ src/yseony/calendar/Calendar.java
@@ -4,22 +4,31 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	public static void main(String[] args) {
-		/*
+	private static final int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDays(int month) {
+		return maxDays[month - 1];
+	}
+
+	public void printSampleCalendar() {
+
 		System.out.println("일  월   화   수   목   금   토");
 		System.out.println("------------------");
 		System.out.println(" 1    2    3   4    5    6    7");
 		System.out.println(" 8   9   10  11   12  13  14");
 		System.out.println("15  16  17  18  19  20  21");
 		System.out.println("22  23 24  25  26  27  28");
-		*/
-		
+
+	}
+
+	public static void main(String[] args) {
+
 		// 숫자를 입력 받아 해당 월의 최대 일수를 출력하는 프로그램
-		int[] maxDays = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		Scanner keyboard = new Scanner(System.in)	;
+		Scanner keyboard = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요.");
 		int month = keyboard.nextInt();
-		System.out.printf("%d월은 %d일까지 있습니다.", month, maxDays[month]);
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDays(month));
 		keyboard.close();
 	}
 
