a8c04fd7e475c1eda51ed36f9d780bbbb54432be
Yeongseon Park <yseon406@gmail.com>
2022-11-04 16:30:56 +0900

    refactor : printSampleCalendar 메소드 수정
    샘플 캘린더를 해당 월에 맞게 출력되는 캘린더 pirntCalendar로 수정

--- src/yseony/calendar/Calendar.java
+++ src/yseony/calendar/Calendar.java
@@ -10,22 +10,24 @@ public class Calendar {
 		return maxDays[month - 1];
 	}
 
-	public void printSampleCalendar() {
-
-		System.out.println("일  월   화   수   목   금   토");
+	public void printCalendar(int month) {
+		int maxDays = getMaxDays(month);
+		System.out.println("일   월   화   수   목   금   토");
 		System.out.println("------------------");
-		System.out.println(" 1    2    3   4    5    6    7");
-		System.out.println(" 8   9   10  11   12  13  14");
-		System.out.println("15  16  17  18  19  20  21");
-		System.out.println("22  23 24  25  26  27  28");
-
+		for(int day = 1; day <= maxDays; day++) {
+			System.out.printf("%3d", day);
+			if(day % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
 
 		Scanner keyboard = new Scanner(System.in);
 		Calendar cal = new Calendar();
-
+		
 		while (true) {
 			System.out.println("월을 입력하세요.");
 			System.out.print("> ");
@@ -38,9 +40,10 @@ public class Calendar {
 			if (month < 1 || month > 12) {
 				System.out.println("1에서 12까지의 숫자를 입력하세요.");
 				continue;
-			}
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDays(month));
+			} 
+			cal.printCalendar(month);
 		}
+		
 		keyboard.close();
 	}
 
